33c93c09eaf830b6089b71c6b4c65d45fffab5b5
Brandon Stark <btstark22@gmail.com>
2019-09-26 20:40:38 -0500

    All files created accept for assets

---
+++ controllers/burgersController.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const burger = require("../models/burger.js");
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        //comes from the front end
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId });
+    });
+});
+
+router.get("/", function (req, res) {
+    console.log("res: "+res);
+    
+    burger.read(function (data) {
+        const burgers = {
+            burgers: data
+        };
+        console.log(burgers);
+        res.render("index", burgers);
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    burger.delete(condition, function (result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
